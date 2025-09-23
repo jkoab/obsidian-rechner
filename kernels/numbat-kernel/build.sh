66c841ee3d9ebfabba7d8da622828683fab1d819
jkoab <69647852+jkoab@users.noreply.github.com>
2025-09-23 17:28:09 +0200

    fix wasm build

--- kernels/numbat-kernel/build.sh
+++ kernels/numbat-kernel/build.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 rm -rf ./pkg
-RUSTFLAGS='--cfg getrandom_backend="wasm_js"' wasm-pack build --target=web --release
 
+wasm-pack --verbose build --target=web --release
 echo "$(date -Iseconds),$(stat -f%z pkg/numbat_kernel_bg.wasm)" >> wasm_size_log.csv
